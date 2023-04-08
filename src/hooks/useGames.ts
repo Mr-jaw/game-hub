9056636e19b684f225cf04bb39f923614ba11140
jaw_breaker <muhammadh1445@gmail.com>
2023-04-08 10:32:46 +0530

    Filter games by platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,19 +2,27 @@ import useData from "./useData";
 import { Genre } from "./useGenres";
 
 export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
+  id: number;
+  name: string;
+  slug: string;
 }
 
 export interface Games {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform }[];
-    metacritic: number;
-  }
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+}
 
-const useGames = (selectedGenre: Genre | null) => useData<Games>('/games', {params: {genres: selectedGenre?.id}}, [selectedGenre?.id])
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatfrom: Platform | null
+) =>
+  useData<Games>(
+    "/games",
+    { params: { genres: selectedGenre?.id, platforms: selectedPlatfrom?.id } },
+    [selectedGenre?.id, selectedPlatfrom?.id]
+  );
 
 export default useGames;
