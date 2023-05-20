03f0db64cc4d5f6573cd4b0b9d812abd10576f06
jaw_breaker <muhammadh1445@gmail.com>
2023-05-20 15:15:06 +0530

    Build game Trailer

---
+++ src/entities/Trailer.ts
@@ -0,0 +1,9 @@
+export interface Trailer {
+  id: string;
+  name: string;
+  preview: string;
+  data: {
+    408: string;
+    max: string;
+  };
+}
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,20 +2,22 @@ import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
-  const { data, isLoading, error } = useGame(slug!);
+  const { data: game, isLoading, error } = useGame(slug!);
   if (isLoading) return <Spinner />;
 
-  if (error || !data) throw error;
+  if (error || !game) throw error;
 
   return (
     <>
-      <Heading>{data.name}</Heading>
-      <ExpandableText>{data.description_raw}</ExpandableText>
-      <GameAttributes data={data} />
+      <Heading>{game.name}</Heading>
+      <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes data={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
